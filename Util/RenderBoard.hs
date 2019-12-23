9902df1e11d5c570cb8cbf609e9cac34ece29742
Uwe Schmidt <uwe@fh-wedel.de>
2019-12-23 14:12:11 +0100

    renderBoard generalized

--- Util/RenderBoard.hs
+++ Util/RenderBoard.hs
@@ -2,24 +2,40 @@ module Util.RenderBoard
   ( renderBoard )
 where
 
-import qualified Data.HashMap.Strict as M
+import qualified Data.HashMap.Strict as HMS
+import qualified Data.Map.Strict     as MS
 
 -- --------------------
 
-type Pos     = (Int, Int)
-type Board a = M.HashMap Pos a
+type Pos       = (Int, Int)
+type Board m a = m Pos a
 
-renderBoard :: Char -> (a -> Char) -> Board a -> [String]
+renderBoard :: Lookup m
+            => Char -> (a -> Char)
+            -> Board m a -> [String]
 renderBoard defC toC board =
   map toRow [minY..maxY]
   where
-    maxX = maximum . map fst . M.keys $ board
-    maxY = maximum . map snd . M.keys $ board
-    minX = minimum . map fst . M.keys $ board
-    minY = minimum . map snd . M.keys $ board
+    maxX = maximum . map fst . keysB $ board
+    maxY = maximum . map snd . keysB $ board
+    minX = minimum . map fst . keysB $ board
+    minY = minimum . map snd . keysB $ board
 
     toRow y = map toChar [minX..maxX]
       where
-        toChar x = maybe defC toC $ M.lookup (x, y) board
+        toChar x = maybe defC toC $ lookupB (x, y) board
+
+class Lookup m where
+  lookupB :: Pos -> m Pos a -> Maybe a
+  keysB   :: m Pos a -> [Pos]
+
+
+instance Lookup HMS.HashMap where
+  lookupB = HMS.lookup
+  keysB   = HMS.keys
+
+instance Lookup MS.Map where
+  lookupB = MS.lookup
+  keysB   = MS.keys
 
 -- --------------------
